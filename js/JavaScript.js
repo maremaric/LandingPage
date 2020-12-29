68d0aa3f5f844b2ff2462fd70d2fb3a65230823a
maremaric <markoreus888@gmail.com>
2020-12-29 12:37:42 +0100

    vezba

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -193,18 +193,18 @@ hamBurger.addEventListener("click", function() {
 
 // Scroll reveal
 
-window.sr = ScrollReveal();
+// window.sr = ScrollReveal();
 
-sr.reveal('.animate-right', {
-    origin: 'right',
-    duration: 1500,
-    distance: '25rem',
-    delay: 600
-});
+// sr.reveal('.animate-right', {
+//     origin: 'right',
+//     duration: 1500,
+//     distance: '25rem',
+//     delay: 600
+// });
 
-sr.reveal('.animate-left', {
-    origin: 'left',
-    duration: 1500,
-    distance: '25rem',
-    delay: 600
-});
+// sr.reveal('.animate-left', {
+//     origin: 'left',
+//     duration: 1500,
+//     distance: '25rem',
+//     delay: 600
+// });
